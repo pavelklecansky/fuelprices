6314308eb43906b3b6884f18b3ce655814606f62
Pavel Klečanský <klecanskypavel@gmail.com>
2023-07-07 16:55:38 +0200

    Remove running fuel price service on run.

--- src/main/java/cz/klecansky/gasprices/FuelPricesApplication.java
+++ src/main/java/cz/klecansky/gasprices/FuelPricesApplication.java
@@ -6,16 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FuelPricesApplication implements CommandLineRunner {
-
-	@Autowired FuelPriceFunction fuelPriceFunction;
+public class FuelPricesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FuelPricesApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
-		fuelPriceFunction.get();
 	}
 }
